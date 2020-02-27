55036b51f316f1ae4f7fcb8a007bf3b38fabdc9b
Marcio <marciofujikura@hotmail.com>
2020-02-27 08:23:21 -0300

    Adicionando o Adapter inicial

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,14 +1,23 @@
 package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_transacoes);
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 100,00", "Salario - R$ 3000,00")
+
+        val arrayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
     }
 
 }
