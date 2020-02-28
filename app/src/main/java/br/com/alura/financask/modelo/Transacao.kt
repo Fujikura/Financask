8f22af3003b0f0d99b7a7fd93314123b2310ac04
Marcio <marciofujikura@hotmail.com>
2020-02-28 08:11:26 -0300

    Criando a classe Transacao

---
+++ app/src/main/java/br/com/alura/financask/modelo/Transacao.kt
@@ -0,0 +1,14 @@
+package br.com.alura.financask.modelo
+
+import java.math.BigDecimal
+import java.util.*
+
+class Transacao(valor: BigDecimal,
+                categoria: String,
+                data: Calendar) {
+
+    private val valor = valor
+    private val categoria = categoria
+    private val data = data
+    
+}
