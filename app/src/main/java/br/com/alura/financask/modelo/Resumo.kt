a2bdc10a0c9dbf792ebdde7d4b7033607e768a9b
Marcio <marciofujikura@hotmail.com>
2020-03-04 18:17:25 -0300

    Uso de Single-Expression ou property

--- app/src/main/java/br/com/alura/financask/modelo/Resumo.kt
+++ app/src/main/java/br/com/alura/financask/modelo/Resumo.kt
@@ -4,18 +4,11 @@ import java.math.BigDecimal
 
 class Resumo(private val transacoes: List<Transacao>) {
 
-    fun receita(): BigDecimal {
-        return somaPor(Tipo.RECEITA)
+    fun receita(): BigDecimal = somaPor(Tipo.RECEITA)
 
-    }
+    fun despesa(): BigDecimal = somaPor(Tipo.DESPESA)
 
-    fun despesa(): BigDecimal {
-        return somaPor(Tipo.DESPESA)
-    }
-
-    fun total(): BigDecimal{
-        return receita().subtract(despesa())
-    }
+    fun total(): BigDecimal = receita().subtract(despesa())
 
     private fun somaPor(tipo: Tipo): BigDecimal{
         val somaDeTransacoesPeloTipo = transacoes.filter { it.tipo == tipo }
