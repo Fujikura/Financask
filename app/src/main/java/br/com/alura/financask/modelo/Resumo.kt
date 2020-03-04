a73f5d918b25aa22e7540708111a0af009fc9bae
Marcio <marciofujikura@hotmail.com>
2020-03-04 18:12:01 -0300

    Extraindo a função de soma por tipo

--- app/src/main/java/br/com/alura/financask/modelo/Resumo.kt
+++ app/src/main/java/br/com/alura/financask/modelo/Resumo.kt
@@ -5,19 +5,21 @@ import java.math.BigDecimal
 class Resumo(private val transacoes: List<Transacao>) {
 
     fun receita(): BigDecimal {
-        val somaDaReceita = transacoes.filter { transacao -> transacao.tipo == Tipo.RECEITA }
-            .sumByDouble { transacao -> transacao.valor.toDouble() }
-        return BigDecimal(somaDaReceita)
+        return somaPor(Tipo.RECEITA)
 
     }
 
     fun despesa(): BigDecimal {
-        val somaDaDespesa = transacoes.filter { transacao -> transacao.tipo == Tipo.DESPESA }
-            .sumByDouble { transacao -> transacao.valor.toDouble() }
-        return BigDecimal(somaDaDespesa)
+        return somaPor(Tipo.DESPESA)
     }
 
     fun total(): BigDecimal{
         return receita().subtract(despesa())
+    }
+
+    private fun somaPor(tipo: Tipo): BigDecimal{
+        val somaDeTransacoesPeloTipo = transacoes.filter { it.tipo == tipo }
+            .sumByDouble { it.valor.toDouble() }
+        return BigDecimal(somaDeTransacoesPeloTipo)
     }
 }
